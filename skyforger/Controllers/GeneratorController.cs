4ea013c2ac54dfc0b8a200cee6a3270a914d4ac9
lperl <luke.perley@economicmodeling.com>
2019-12-21 08:34:24 -0800

    initial commit

---
+++ skyforger/Controllers/GeneratorController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace skyforger.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GeneratorController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<GeneratorController> _logger;
+
+        public GeneratorController(ILogger<GeneratorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("test")]
+        public async Task<ActionResult> Test()
+        {
+            var rng = new Random();
+            return Ok(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Summaries[rng.Next(Summaries.Length)]
+                })
+                .ToArray());
+        }
+    }
+}
---
+++ skyforger/Program.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace skyforger
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            // Tailor environment variable (empty string catch)
+            if (string.IsNullOrWhiteSpace(environment))
+            {
+                environment = "development";
+            }
+
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddCommandLine(args)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{environment.ToLower()}.json", optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+            CreateHostBuilder(args, config).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args, IConfigurationRoot config) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseConfiguration(config);
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
